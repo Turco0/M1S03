cade2c1817e8899920db77e5b1c82a173c8c9bf1
Iedo <iedo2298@gmail.com>
2023-02-16 18:40:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 let marcasDeCarro = [{ marca: "Audi" }, { marca: "BMW" }];
 
+let modelosDeCarro = [{ modelo: "A4" }, { modelo: "A5" }];
+
+function adicionarModeloCarro(){
+const valor = document.querySelector('#modelo').value;
+let itemNovo = new Array
+itemNovo.modelo = valor;
+modelosDeCarro.push(itemNovo);
+window.alert("Modelo adicionado com sucesso");
+for (let modelo of modelosDeCarro) {
+console.log(modelo.modelo);
+}
+}
+
 function adicionarMarcaCarro() {
   const valor = document.querySelector('#marca').value;
   let itemNovo = new Array
