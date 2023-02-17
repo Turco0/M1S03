a00ab8cddb11ddbb52f13e4cdb5518e0e9db2ea1
Iedo <iedo2298@gmail.com>
2023-02-17 18:44:55 -0300

    ex9

--- script.js
+++ script.js
@@ -1,60 +1,83 @@
-let marcasDeCarro = [{ marca: "Audi" }, { marca: "BMW" }];
-
-let modelosDeCarro = [{ modelo: "A4" }, { modelo: "A5" }];
-
-let veiculos = [{ modelo: "A4", marca: "Audi", km: 0, valor: 5000 , cor: 'Verde'}, { modelo: "A5", marca: "BMW", km: 1555, valor: 10000, cor: 'Azul'}]
-
-for (modelo of modelosDeCarro) {
-let opcaomodelo = document.querySelector("#modelo-veiculo");
-let option = document.createElement("option");
-option.innerHTML = modelo.modelo;
-option.value = modelo.modelo;
-opcaomodelo.appendChild(option);
-}
-for (marca of marcasDeCarro) {
-let opcaomarca = document.querySelector("#marca-veiculo");
-let option = document.createElement("option");
-option.innerHTML = marca.marca;
-option.value = marca.marca;
-opcaomarca.appendChild(option);
+function adicionarModeloCarro() {
+  let modelosDeCarro = pegarModelosDeCarro();
+  const valor = document.querySelector("#modelo").value;
+  let itemNovo = "";
+  itemNovo = valor;
+  modelosDeCarro.push(itemNovo);
+  salvarModelosDeCarro(modelosDeCarro);
+  window.alert("Modelo adicionado com sucesso");
 }
 
-function adicionarModeloCarro(){
-const valor = document.querySelector('#modelo').value;
-let itemNovo = new Array
-itemNovo.modelo = valor;
-modelosDeCarro.push(itemNovo);
-window.alert("Modelo adicionado com sucesso");
-for (let modelo of modelosDeCarro) {
-console.log(modelo.modelo);
-}
-}
-
-function adicionarVeiculo(){
-let modelo = document.getElementById("modelo-veiculo").value;
-let marca = document.getElementById("marca-veiculo").value;
-let km = document.getElementById("km-veiculo").value;
-let valor = document.getElementById("valor-veiculo").value;
-let cor = document.getElementById("cor-veiculo").value;
-let veiculo = { modelo: modelo, marca: marca, km: km, valor:valor, cor: cor};
-veiculos.push(veiculo);
-window.alert("Veiculo adicionado com sucesso");
-console.log(veiculos);
+function adicionarVeiculo() {
+  let veiculos = pegarVeiculos();
+  let imagem = document.getElementById("imagem-veiculo").value;
+  let modelo = document.getElementById("modelo-veiculo").value;
+  let marca = document.getElementById("marca-veiculo").value;
+  let km = document.getElementById("km-veiculo").value;
+  let valor = document.getElementById("valor-veiculo").value;
+  let cor = document.getElementById("cor-veiculo").value;
+  let veiculo = {
+    modelo: modelo,
+    marca: marca,
+    km: km,
+    valor: valor,
+    cor: cor,
+    imagem: imagem,
+  };
+  veiculos.push(veiculo);
+  salvarVeiculos(veiculos);
+  window.alert("Veiculo adicionado com sucesso");
 }
 
 function adicionarMarcaCarro() {
-  const valor = document.querySelector('#marca').value;
-  let itemNovo = new Array
-  itemNovo.marca = valor;
+  let marcasDeCarro = pegarMarcasDeCarro();
+  const valor = document.querySelector("#marca").value;
+  let itemNovo = "";
+  itemNovo = valor;
   marcasDeCarro.push(itemNovo);
+  salvarMarcasDeCarro(marcasDeCarro);
   window.alert("Marca adicionada com sucesso");
-  for (let marca of marcasDeCarro) {
-    console.log(marca.marca);
-  }
 }
 
 function listarVeiculos() {
-for (let veiculo of veiculos) {
-console.log(`${veiculo.modelo} - ${veiculo.marca} - ${veiculo.km}KM - Valor: R$${veiculo.valor} - Cor: ${veiculo.cor}`);
+  let veiculos = pegarVeiculos();
+  for (let veiculo of veiculos) {
+    let comando = `
+    <div class="carro">
+      <img src="${veiculo.imagem}" alt="">
+      <ul>
+        <li>${veiculo.modelo}</li>
+        <li>${veiculo.marca}</li>
+        <li>${veiculo.valor}</li>
+        <li>${veiculo.km}</li>
+        <li>${veiculo.cor}</li>
+      </ul>
+    </div>    
+    `;
+    document.querySelector("#patio").innerHTML += comando;
+  }
 }
+
+function salvarMarcasDeCarro(marcasDeCarro) {
+  localStorage.setItem("marcasDeCarro", JSON.stringify(marcasDeCarro));
+}
+
+function salvarModelosDeCarro(modelosDeCarro) {
+  localStorage.setItem("modelosDeCarro", JSON.stringify(modelosDeCarro));
+}
+
+function salvarVeiculos(veiculos) {
+  localStorage.setItem("veiculos", JSON.stringify(veiculos));
+}
+
+function pegarModelosDeCarro() {
+  return JSON.parse(localStorage.getItem("modelosDeCarro"));
+}
+
+function pegarMarcasDeCarro() {
+  return JSON.parse(localStorage.getItem("marcasDeCarro"));
+}
+
+function pegarVeiculos() {
+  return JSON.parse(localStorage.getItem("veiculos"));
 }
